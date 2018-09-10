2fd6edfe4426149101996a21a16f2f5f053d959c
Logan <gryphontooth@gmail.com>
2018-09-10 12:42:38 -0400

    Initial Commit

---
+++ app/src/main/java/com/loroad/seg2105_final/MainActivity.java
@@ -0,0 +1,13 @@
+package com.loroad.seg2105_final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
