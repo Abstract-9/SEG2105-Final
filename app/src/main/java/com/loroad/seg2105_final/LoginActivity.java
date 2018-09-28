d60cd2376e74a32e4de66d4dbb8748488a2ca3c7
Logan Rodie <lrodi045@uottawa.ca>
2018-09-27 21:24:34 -0400

    Added sign up intent

--- app/src/main/java/com/loroad/seg2105_final/LoginActivity.java
+++ app/src/main/java/com/loroad/seg2105_final/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.loroad.seg2105_final;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends Activity {
 
@@ -9,5 +11,12 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.signUpLink);
+
+    }
+
+    public void signUpListener(View v){
+        Intent signUp = new Intent(this, SignupActivity.class);
+        startActivity(signUp);
     }
 }
