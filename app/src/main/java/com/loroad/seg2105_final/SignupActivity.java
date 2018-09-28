a8b30598030b6defdce6f157beccf45ccb28c641
Logan Rodie <lrodi045@uottawa.ca>
2018-09-28 11:38:37 -0400

    Added user type spinner

--- app/src/main/java/com/loroad/seg2105_final/SignupActivity.java
+++ app/src/main/java/com/loroad/seg2105_final/SignupActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SignupActivity extends Activity {
 
@@ -11,6 +13,12 @@ public class SignupActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //Set up spinner
+        Spinner userChoices = (Spinner) findViewById(R.id.userType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.userChoices, android.R.layout.simple_spinner_item);
+        userChoices.setAdapter(adapter);
     }
 
     public void loginListener(View v){
