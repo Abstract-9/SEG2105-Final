82e060e9f4a7f309227daa3d5fcd1a2d6398a143
Logan Rodie <lrodi045@uottawa.ca>
2018-10-04 17:48:19 -0400

    Finished FireBase integration for login page

--- app/src/main/java/com/codeflo/seg2105_final/LoginActivity.java
+++ app/src/main/java/com/codeflo/seg2105_final/LoginActivity.java
@@ -3,9 +3,19 @@ package com.codeflo.seg2105_final;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
+import java.util.Map;
 
 public class LoginActivity extends Activity {
 
@@ -24,6 +34,29 @@ public class LoginActivity extends Activity {
         startActivity(signUp);
     }
 
+    OnCompleteListener<QuerySnapshot> loginListener = new OnCompleteListener<QuerySnapshot>() {
+        @Override
+        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+            if(task.getResult()!=null) {
+                TextView userPass = (TextView) findViewById(R.id.password);
+                QuerySnapshot result = task.getResult();
+                DocumentSnapshot user = result.getDocuments().get(0);
+                if(user.get("Password").equals(userPass.getText())){
+                    //TODO authentication successful
+                }else{
+                    //TODO unknown user
+                }
+            }else{
+                //TODO throw error
+            }
+        }
+    };
+
     public void onLogin(View v){
+        TextView userInput = (TextView) findViewById(R.id.userName);
+
+        db = FirebaseFirestore.getInstance();
+        db.collection("users").whereEqualTo("Username", userInput.getText())
+                .get().addOnCompleteListener(loginListener);
     }
 }
