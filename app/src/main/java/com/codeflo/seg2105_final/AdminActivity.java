c3be60daf5b4c83dbc4825abd7523f3799e310b3
jfan <sleepiesheepy@gmail.com>
2018-11-07 20:29:02 -0500

    2lines of code

--- app/src/main/java/com/codeflo/seg2105_final/AdminActivity.java
+++ app/src/main/java/com/codeflo/seg2105_final/AdminActivity.java
@@ -2,6 +2,12 @@ package com.codeflo.seg2105_final;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.codeflo.seg2105_final.adapters.ServiceAdapter;
+import com.codeflo.seg2105_final.models.Service;
+
+import java.util.ArrayList;
 
 public class AdminActivity extends Activity {
 
@@ -9,5 +15,13 @@ public class AdminActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        ListView listServices = (ListView)findViewById(R.id.serviceList);
+        ServiceAdapter serviceAdapter = new ServiceAdapter(getApplicationContext(), new ArrayList<Service>());
+        listServices.setAdapter(serviceAdapter);
+
+
     }
+
+
 }
