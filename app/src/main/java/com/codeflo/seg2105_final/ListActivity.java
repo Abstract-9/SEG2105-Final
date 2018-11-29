3e97ced25b104384384a5c6fd8370f33f3f9bbc5
Logan Rodie <lrodi045@uottawa.ca>
2018-11-28 22:14:50 -0500

    Started homeOwner implementation

--- app/src/main/java/com/codeflo/seg2105_final/ListActivity.java
+++ app/src/main/java/com/codeflo/seg2105_final/ListActivity.java
@@ -1,13 +1,57 @@
 package com.codeflo.seg2105_final;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 
 public class ListActivity extends Activity {
+
+    boolean byName;
+
+    FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        TextView searchBar = (TextView) findViewById(searchBar);
+        searchBar.setText(getIntent().getStringExtra("search"));
+        byName = getIntent().getBooleanExtra("byName", false);
+
+        db = FirebaseFirestore.getInstance();
+
+        if(byName){
+            db.collection("users").whereEqualTo()
+        }
+
     }
+
+    public void searchBarCLicked(View v){
+        finish();
+    }
+
+
+
+    OnCompleteListener<QuerySnapshot> serviceSearch = new OnCompleteListener<QuerySnapshot>() {
+        @Override
+        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+            if(task.isSuccessful() && task.getResult()!=null){
+                for (DocumentSnapshot doc : task.getResult().getDocuments()) {
+                    if(doc.get)
+                }
+            }
+        }
+    };
 }
--- app/src/main/java/com/codeflo/seg2105_final/SearchActivity.java
+++ app/src/main/java/com/codeflo/seg2105_final/SearchActivity.java
@@ -1,7 +1,16 @@
 package com.codeflo.seg2105_final;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class SearchActivity extends Activity {
 
@@ -10,4 +19,20 @@ public class SearchActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
     }
+
+
+
+    public void search(View v){
+        TextView search = findViewById(R.id.searchBar);
+
+        if(!search.getText().toString().equals("")) {
+            Intent intent = new Intent(this, ListActivity.class);
+            intent.putExtra("search", search.getText().toString());
+            intent.putExtra("byName", byName);
+            startActivity(intent);
+        }
+
+
+    }
+}
 }
